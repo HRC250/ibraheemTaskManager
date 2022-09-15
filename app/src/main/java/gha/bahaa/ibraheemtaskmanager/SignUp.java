2d9b29e809ad7a137ba024c34b5fe4c9fce714cf
Owner <noran@danon.tzafonet.org.il>
2022-09-15 11:36:41 +0300

    the firebase is added
    we finshed Splash screen
    next time test it

---
+++ app/src/main/java/gha/bahaa/ibraheemtaskmanager/SignUp.java
@@ -0,0 +1,32 @@
+package gha.bahaa.ibraheemtaskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class SignUp extends AppCompatActivity {
+
+    TextInputEditText etEmail2;
+    TextInputEditText etPassword2;
+    TextInputEditText etConfirm;
+    Button ButtonSave;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        etEmail2=findViewById(R.id.etEmail2);
+        etPassword2=findViewById(R.id.etPassword2);
+        etConfirm=findViewById(R.id.etConfirm);
+        ButtonSave=findViewById(R.id.ButtonSave);
+
+
+
+    }
+}
--- app/src/main/java/gha/bahaa/ibraheemtaskmanager/sign_in.java
+++ app/src/main/java/gha/bahaa/ibraheemtaskmanager/sign_in.java
@@ -3,12 +3,35 @@ package gha.bahaa.ibraheemtaskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class sign_in extends AppCompatActivity {
+
+    TextInputEditText EtEmail;
+    TextInputEditText Etpass;
+    Button ButtonSignUp;
+    Button ButtonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in2);
+
+        EtEmail=findViewById(R.id.EtEmail);
+        Etpass=findViewById((R.id.Etpass));
+        ButtonSignUp=findViewById(R.id.ButtonSignUp);
+        ButtonSignIn=findViewById(R.id.ButtonSignIn);
+
+        ButtonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
