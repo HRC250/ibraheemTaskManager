0cd3ee0e0fd0009ae2287649e3fcb88284b54499
Owner <noran@danon.tzafonet.org.il>
2022-09-27 13:21:50 +0300

    the firebase is added
    we finshed Splash screen
    next time test it

--- app/src/main/java/gha/bahaa/ibraheemtaskmanager/MainActivity.java
+++ app/src/main/java/gha/bahaa/ibraheemtaskmanager/MainActivity.java
@@ -1,8 +1,13 @@
 package gha.bahaa.ibraheemtaskmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.audiofx.Equalizer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);////
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.itmStettings)
+        {
+            Intent i=new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(i);
+
+        }
+        return true;
+
+        if(item)
+    }
+    }
